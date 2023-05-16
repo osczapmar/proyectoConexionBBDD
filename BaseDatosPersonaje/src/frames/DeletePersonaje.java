584b4211a25d236cc0b1b613a0dda8e9c52f0bff
ausias <ausias@hINFO6-2.xiam.cat>
2023-05-16 13:18:48 +0200

    Proyecto Oscar, parte 1

--- BaseDatosPersonaje/src/frames/DeletePersonaje.java
+++ BaseDatosPersonaje/src/frames/DeletePersonaje.java
@@ -2,29 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package basedatospersonaje;
-
-import java.sql.SQLException;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+package frames;
 
 /**
  *
- * @author joan y oscar
+ * @author ausias
  */
-public class JFrame extends javax.swing.JFrame {
-
-    static final String USER = "a22joaguesan_proy";
-    static final String PWD = "proyectoBD1";
-    static final String URL = "labs.inspedralbes.cat";
-    static final String PORT = "3306";
-    static final String BD_NAME = "a22joaguesa_proyecto";
+public class DeletePersonaje extends javax.swing.JFrame {
 
     /**
-     * Creates new form JFrame
+     * Creates new form DeletePersonaje
      */
-    public JFrame() {
+    public DeletePersonaje() {
         initComponents();
     }
 
@@ -43,11 +32,11 @@ public class JFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 667, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 451, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -57,7 +46,6 @@ public class JFrame extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -71,36 +59,22 @@ public class JFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeletePersonaje.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeletePersonaje.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeletePersonaje.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeletePersonaje.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new JFrame().setVisible(true);
-                try {
-                    Scanner scan = new Scanner(System.in);
-
-                    //Crear la connexió a la BD
-                    BDConnection bdCon = new BDConnection(URL, PORT, BD_NAME, USER, PWD);
-                    bdCon.closeConnection();
-                } catch (NullConnectionException ex) {
-                    Logger.getLogger(JFrame.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (ClassNotFoundException ex) {
-                    Logger.getLogger(JFrame.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (SQLException ex) {
-                    Logger.getLogger(JFrame.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                new DeletePersonaje().setVisible(true);
             }
-        }
-        );
+        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
